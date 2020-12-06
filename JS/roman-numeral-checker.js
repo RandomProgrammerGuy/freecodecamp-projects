79850766b8d52f6c5d15584df1864821ecf974db
RandomProgrammerGuy <parsa_farjam@yahoo.com>
2020-12-06 23:04:13 +0330

    added code for numbers 11-20

--- JS/roman-numeral-checker.js
+++ JS/roman-numeral-checker.js
@@ -22,8 +22,32 @@ function convertToRoman(num) {
             case 10:
                 return 'X'
         }
+    }
 
-
+    if (num > 10 && num <= 20) {
+        num -= 10
+        switch (num){
+            case 1:
+                return 'XI';
+            case 2:
+                return 'XII';
+            case 3:
+                return 'XIII';
+            case 4:
+                return 'XIV';
+            case 5:
+                return 'XV';
+            case 6:
+                return 'XVI';
+            case 7:
+                return 'XVII';
+            case 8:
+                return 'XVIII';
+            case 9:
+                return 'XIX'
+            case 10:
+                return 'XX'
+        }
     }
    }
    
