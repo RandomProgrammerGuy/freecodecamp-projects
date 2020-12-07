acd6819eb95923641f8337daee61cb2bdd494dc5
RandomProgrammerGuy <parsa_farjam@yahoo.com>
2020-12-07 08:01:27 +0330

    added function for numbers 1-10

--- JS/roman-numeral-checker.js
+++ JS/roman-numeral-checker.js
@@ -1,28 +1,33 @@
-function convertToRoman(num) {
-    if (num <= 10) {
-        switch (num){
-            case 1:
-                return 'I';
-            case 2:
-                return 'II';
-            case 3:
-                return 'III';
-            case 4:
-                return 'IV';
-            case 5:
-                return 'V';
-            case 6:
-                return 'VI';
-            case 7:
-                return 'VII';
-            case 8:
-                return 'VIII';
-            case 9:
-                return 'IX'
-            case 10:
-                return 'X'
-        }
+function switchTen(inp) {
+    switch (inp){
+        case 1:
+            return 'I';
+        case 2:
+            return 'II';
+        case 3:
+            return 'III';
+        case 4:
+            return 'IV';
+        case 5:
+            return 'V';
+        case 6:
+            return 'VI';
+        case 7:
+            return 'VII';
+        case 8:
+            return 'VIII';
+        case 9:
+            return 'IX';
+        case 10:
+            return 'X';
     }
+}   
+
+function convertToRoman(num) {
+    if (num <= 10) {        
+            var ans = switchTen(num);
+            return ans;
+        }
 
     if (num > 10 && num <= 20) {
         num -= 10
@@ -49,6 +54,7 @@ function convertToRoman(num) {
                 return 'XX'
         }
     }
-   }
+}
    
-   console.log(convertToRoman(6));
+   
+   console.log(convertToRoman(2));
