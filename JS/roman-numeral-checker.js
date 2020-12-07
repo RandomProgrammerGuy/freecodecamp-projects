f653e948f2ddbc501bdb22d62c96311868751e3d
RandomProgrammerGuy <parsa_farjam@yahoo.com>
2020-12-07 13:44:10 +0330

    wrote previous code in function form

--- JS/roman-numeral-checker.js
+++ JS/roman-numeral-checker.js
@@ -23,68 +23,93 @@ function switchTen(inp) {
     }
 }   
 
+function switchHundred(num) {
+    if (num > 10 && num <= 20) {
+        tenth = 'X'
+        one = String(switchTen(num - 10))
+    }
+
+    else if (num > 20 && num <= 30) {
+        tenth = 'XX';
+        one = String(switchTen(num - 20));
+    }
+
+    else if (num > 30 && num <= 40) {
+        tenth = 'XXX';
+        one = String(switchTen(num - 30));
+    }
+
+    else if (num > 40 && num <= 50) {
+        tenth = 'XL';
+        one = String(switchTen(num - 40));
+    }
+
+    else if (num > 50 && num <= 60) {
+        tenth = 'L';
+        one = String(switchTen(num - 50));
+    }
+
+    else if (num > 60 && num <= 70) {
+        tenth = 'LX';
+        one = String(switchTen(num - 60));
+    }
+
+    else if (num > 70 && num <= 80) {
+        tenth = 'LXX';
+        one = String(switchTen(num - 70));
+    }
+
+    else if (num > 80 && num <= 90) {
+        tenth = 'LXXX';
+        one = String(switchTen(num - 80));
+    }
+
+    else if (num > 90 && num <= 100) {
+        tenth = 'XC';
+        one = String(switchTen(num - 90));
+    }
+
+    else if (num > 100 && num <= 110) {
+        tenth = 'C';
+        one = String(switchTen(num - 40));
+    }
+}
+
 function convertToRoman(num) {
     var one = '';
     var tenth = '';
     var hundredth = '';
     var thousand = '';
 
-        if (num <= 10) {        
-            one = switchTen(num);
+    if (num <= 10) {
+        one = switchTen();
+    } else if (num <= 110) {
+        switchHundred(num);
+    } else if (num > 110 && num <= 1000) {
+        switch (num) {
+            case 200:
+                one = 'CC'
+            case 300:
+                one = 'CCC'
+            case 400:
+                one = 'CD'
+            case 500:
+                one = 'D'
+            case 600:
+                one = 'DC'
+            case 700:
+                one = 'DCC'
+            case 800:
+                one = 'DCCC'
+            case 900:
+                one = 'CM'
+            case 1000:
+                one = 'M'
         }
-
-        if (num > 10 && num <= 20) {
-            tenth = 'X'
-            one = String(switchTen(num - 10))
-        }
-
-        if (num > 20 && num <= 30) {
-            tenth = 'XX';
-            one = String(switchTen(num - 20));
-        }
-
-        if (num > 30 && num <= 40) {
-            tenth = 'XXX';
-            one = String(switchTen(num - 30));
-        }
-
-        if (num > 40 && num <= 50) {
-            tenth = 'XL';
-            one = String(switchTen(num - 40));
-        }
-
-        if (num > 50 && num <= 60) {
-            tenth = 'L';
-            one = String(switchTen(num - 50));
-        }
-
-        if (num > 60 && num <= 70) {
-            tenth = 'LX';
-            one = String(switchTen(num - 60));
-        }
-
-        if (num > 70 && num <= 80) {
-            tenth = 'LXX';
-            one = String(switchTen(num - 70));
-        }
-
-        if (num > 80 && num <= 90) {
-            tenth = 'LXXX';
-            one = String(switchTen(num - 80));
-        }
-
-        if (num > 90 && num <= 100) {
-            tenth = 'XC';
-            one = String(switchTen(num - 90));
-        }
-
-        if (num > 100 && num <= 110) {
-            tenth = 'C';
-            one = String(switchTen(num - 40));
-        }
+    }
 
     var ans = thousand + hundredth + tenth + one;
     return ans;
 }
    
-   console.log(convertToRoman(27));
+   console.log(convertToRoman(80));
