b61f2fece65a198a25388fc59c3d676d74f51ee0
RandomProgrammerGuy <parsa_farjam@yahoo.com>
2020-12-07 14:00:07 +0330

    added array imcluding basic roman numerals

--- JS/roman-numeral-checker.js
+++ JS/roman-numeral-checker.js
@@ -1,3 +1,23 @@
+romanNumerals = {
+    'I': 1, 
+    'II': 2,
+    'III': 3,
+    'IV': 4,
+    'V': 5, 
+    'VI': 6,
+    'VII': 7,
+    'VIII': 8,
+    'IX': 9,
+    'X': 10,
+    'IL': 49, 
+    'L': 50,
+    'IC': 99, 
+    'C': 100,
+    'CD': 400, 
+    'D': 500,
+    'CM': 900, 
+    'M': 1000
+}
 function convertToRoman(num) {
     return num;
    }
