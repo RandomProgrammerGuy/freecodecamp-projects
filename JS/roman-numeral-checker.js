e36e200047aeb6afa692d48a9893efb86b297950
RandomProgrammerGuy <parsa_farjam@yahoo.com>
2020-12-07 16:53:25 +0330

    bugfixes and code cleanup

--- JS/roman-numeral-checker.js
+++ JS/roman-numeral-checker.js
@@ -1,70 +1,5 @@
-var decimals = [
-    1,
-    2,
-    3,
-    4,
-    5,
-    6,
-    7,
-    8,
-    9,
-    10,
-    11,
-    20,
-    30,
-    40,
-    50,
-    60,
-    70,
-    80,
-    90,
-    100,
-    200,
-    300,
-    400,
-    500,
-    600,
-    700,
-    800,
-    900,
-    1000,
-    2000,
-    3000
-]
-
-var romans = [
-    'I',
-    'II',
-    'III',
-    'IV',
-    'V',
-    'VI',
-    'VII',
-    'VIII',
-    'IX',
-    'X',
-    'XI',
-    'XX',
-    'XXX',
-    'XL',
-    'L',
-    'LX',
-    'LXX',
-    'LXXX',
-    'XC',
-    'C',
-    'CC',
-    'CCC',
-    'CD',
-    'D',
-    'DC',
-    'DCC',
-    'DCCC',
-    'CM',
-    'M',
-    'MM',
-    'MMM'
-]
+var romans = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"];
+var decimals = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
 
 var ans = '';
 
@@ -78,5 +13,5 @@ function convertToRoman(num) {
     return ans;
    }
    
-   console.log(convertToRoman(2));
+   console.log(convertToRoman(300));
    
