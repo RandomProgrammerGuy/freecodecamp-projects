8ec33d5a474c685a23e468769c99d864090ab2d4
RandomProgrammerGuy <parsa_farjam@yahoo.com>
2020-12-07 14:53:02 +0330

    finished code

--- JS/roman-numeral-checker.js
+++ JS/roman-numeral-checker.js
@@ -1,40 +1,80 @@
-romanNumerals = {
-    1: 'I',
-    2: 'II',
-    3: 'III',
-    4: 'IV',
-    5: 'V',
-    6: 'VI',
-    7: 'VII',
-    8: 'VIII',
-    9: 'IX',
-    10: 'X',
-    11: 'IX',
-    20: 'XX',
-    30: 'XXX',
-    40: 'XL',
-    50: 'L',
-    60: 'LX',
-    70: 'LXX',
-    80: 'LXXX',
-    90: 'XC',
-    100: 'C',
-    200: 'CC',
-    300: 'CCC',
-    400: 'CD',
-    500: 'D',
-    600: 'DC',
-    700: 'DCC',
-    800: 'DCCC',
-    900: 'CM',
-    1000: 'M',
-    2000: 'MM',
-    3000: 'MMM'
-}
+var decimals = [
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    11,
+    20,
+    30,
+    40,
+    50,
+    60,
+    70,
+    80,
+    90,
+    100,
+    200,
+    300,
+    400,
+    500,
+    600,
+    700,
+    800,
+    900,
+    1000,
+    2000,
+    3000
+]
+
+var romans = [
+    'I',
+    'II',
+    'III',
+    'IV',
+    'V',
+    'VI',
+    'VII',
+    'VIII',
+    'IX',
+    'X',
+    'XI',
+    'XX',
+    'XXX',
+    'XL',
+    'L',
+    'LX',
+    'LXX',
+    'LXXX',
+    'XC',
+    'C',
+    'CC',
+    'CCC',
+    'CD',
+    'D',
+    'DC',
+    'DCC',
+    'DCCC',
+    'CM',
+    'M',
+    'MM',
+    'MMM'
+]
 
 function convertToRoman(num) {
+    for (var index = 0; index < decimals.length; index++) {
+        while (decimals[index] <= num) {
+          romanized += romans[index];
+          num -= decimals[index];
+        }
+      }
     return num;
    }
    
-   convertToRoman(36);
+   console.log(convertToRoman(2));
    
